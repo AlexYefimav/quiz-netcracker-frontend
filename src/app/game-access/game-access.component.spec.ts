d554fd43356c50452e912dbee59b56dd20d922d0
Polina <57046129+pollishullya@users.noreply.github.com>
2021-02-03 00:41:13 +0300

    Game-access component

---
+++ src/app/game-access/game-access.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameAccessComponent } from './game-access.component';
+
+describe('GameAccessComponent', () => {
+  let component: GameAccessComponent;
+  let fixture: ComponentFixture<GameAccessComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GameAccessComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GameAccessComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
